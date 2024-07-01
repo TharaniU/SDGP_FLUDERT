b35c9b366b985cd58bd309e1364b5705fae09b39
TharaniU <125527052+TharaniU@users.noreply.github.com>
2024-07-02 04:14:08 +0530

    changes

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -20,6 +20,22 @@ class _SignUpScreenState extends State<SignUpScreen> {
             child: SizedBox(
               height: 10,
             ),
+          ),
+          Expanded(
+            flex: 7,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(25.0, 50.0, 25.0, 20.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(40.0),
+                ),
+              ),
+              child: SingleChildScrollView(
+                
+              ),
+            ),
           )
         ],
       )
