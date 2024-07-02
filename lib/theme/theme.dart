0912ba63330f8784af0b9853026785aaa3a12363
TharaniU <125527052+TharaniU@users.noreply.github.com>
2024-07-03 03:18:23 +0530

    themes changes

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -7,7 +7,53 @@ const lightColorScheme = ColorScheme(
   secondary: Color(0xFF6EAEE7),
   onSecondary: Color(0xFFFFFFF),
   error: Color(0xFFBA1A1A),
-  surface: Color(0xFFFCFDF6),
-  onBackground: Color(value)
-  
-)
+  background: Color(0xFFFCFDF6),
+  onBackground: Color(0xFF1A1C18),
+  shadow: Color(0xFF000000),
+  outlineVariant: Color(0xFFC2C8BC),
+  surface: Color(0xFFF9FAF3),
+  onSurface: Color(0xFF1A1C18),
+);
+
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF416FDF),
+  onPrimary: Color(0xFFFFFF),
+  secondary: Color(0xFF6EAEE7),
+  onSecondary: Color(0xFFFFFFF),
+  error: Color(0xFFBA1A1A),
+  background: Color(0xFFFCFDF6),
+  onBackground: Color(0xFF1A1C18),
+  shadow: Color(0xFF000000),
+  outlineVariant: Color(0xFFC2C8BC),
+  surface: Color(0xFFF9FAF3),
+  onSurface: Color(0xFF1A1C18),
+);
+
+ThemeData lightMode = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.light,
+  colorScheme: lightColorScheme,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(
+        lightColorScheme.primary,
+      ),
+      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      elevation: MaterialStateProperty.all<double>(5.0),
+      padding: MaterialStateProperty.all<EdgeInsets>(
+          const EdgeInsets.symmetric(horizontal: 20, vertical: 18)),
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+    ),
+  ),
+);
+
+ThemeData darkMode = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.dark,
+  colorScheme: darkColorScheme,
+);
