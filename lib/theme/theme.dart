de6b36f78af056f7b078b357f59e1a08a5881e18
TharaniU <125527052+TharaniU@users.noreply.github.com>
2024-07-03 01:27:24 +0530

    theme updates

---
+++ lib/theme/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF416FDF),
+  onPrimary: Color(0xFFFFFF),
+  secondary: Color(0xFF6EAEE7),
+  onSecondary: Color(0xFFFFFFF),
+  error: Color(0xFFBA1A1A),
+  surface: Color(0xFFFCFDF6),
+  onBackground: Color(value)
+  
+)
