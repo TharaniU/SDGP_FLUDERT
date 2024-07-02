cbf80e22d5e5231acebd890ada30ae288f6db313
TharaniU <125527052+TharaniU@users.noreply.github.com>
2024-07-02 18:11:03 +0530

    update signin screen

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -50,15 +50,88 @@ class _SignInScreenState extends State<SigninScreen> {
                               height: 40.0,
                             ),
                             TextFormField(
-                                validator: (value) {
-                                  if (value == null || value.isNotEmpty) {
-                                    return 'Please enter your Email';
-                                  }
-                                  return null;
-                                },
-                                decoration: InputDecoration(
-                                  label: const Text('Email'),
-                                ))
+                              validator: (value) {
+                                if (value == null || value.isNotEmpty) {
+                                  return 'Please enter your Email';
+                                }
+                                return null;
+                              },
+                              decoration: InputDecoration(
+                                label: const Text('Email'),
+                                hintText: 'Enter Email',
+                                hintStyle: const TextStyle(
+                                  color: Colors.black26,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: Colors.black12,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 25.0,
+                            ),
+                            TextFormField(
+                              obscureText: true,
+                              obscuringCharacter: '*',
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please Enter Password';
+                                }
+                                return null;
+                              },
+                              decoration: InputDecoration(
+                                label: const Text('Password'),
+                                hintText: 'Enter Password',
+                                helperStyle: const TextStyle(
+                                  color: Colors.black26,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: Colors.black12,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 25.0,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Checkbox(
+                                      value: rememberPassword,
+                                      onChanged: (bool? value) {
+                                        setState(() {
+                                          rememberPassword = value!;
+                                        });
+                                      },
+                                      activeColor: lightColorScheme.primary,
+                                    ),
+                                    const Text(
+                                      'Remember Me',
+                                      style: TextStyle(
+                                        color: Colors.black45,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                GestureDetector(
+                                  child: Text(
+                                    'Forget Password?',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: lightClorScheme.primary,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ],
                         ))),
               ))
